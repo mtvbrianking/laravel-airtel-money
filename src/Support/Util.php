8d2e0cbeb15b29701b1885d86df55531fd8c3c71
mtvbrianking <mtvbrianking@gmail.com>
2025-10-30 22:33:42 +0300

    versions supported

--- src/Support/Util.php
+++ src/Support/Util.php
@@ -13,7 +13,6 @@ use GuzzleHttp\MessageFormatter;
 use GuzzleHttp\Middleware;
 use Illuminate\Container\Container;
 use Illuminate\Support\Str;
-use Psr\Log\LogLevel;
 
 class Util
 {
@@ -45,13 +44,13 @@ class Util
         $id = $_SERVER['REQUEST_ID'] ?? Str::random(10);
 
         $messageFormats = [
-            "HTTP_OUT_{$id} [Request] {method} {target}" => LogLevel::INFO,
-            "HTTP_OUT_{$id} [Request] [Headers] \n{req_headers}" => LogLevel::DEBUG,
-            "HTTP_OUT_{$id} [Request] [Body] {req_body}" => LogLevel::DEBUG,
-            "HTTP_OUT_{$id} [Response] HTTP/{version} {code} {phrase} Size: {res_header_Content-Length}" => LogLevel::INFO,
-            "HTTP_OUT_{$id} [Response] [Headers] \n{res_headers}" => LogLevel::DEBUG,
-            "HTTP_OUT_{$id} [Response] [Body] {res_body}" => LogLevel::DEBUG,
-            // "HTTP_OUT_{$id} [Error] {error}" => LogLevel::ERROR,
+            "HTTP_OUT_{$id} [Request] {method} {target}" => 'info',
+            "HTTP_OUT_{$id} [Request] [Headers] \n{req_headers}" => 'debug',
+            "HTTP_OUT_{$id} [Request] [Body] {req_body}" => 'debug',
+            "HTTP_OUT_{$id} [Response] HTTP/{version} {code} {phrase} Size: {res_header_Content-Length}" => 'info',
+            "HTTP_OUT_{$id} [Response] [Headers] \n{res_headers}" => 'debug',
+            "HTTP_OUT_{$id} [Response] [Body] {res_body}" => 'debug',
+            // "HTTP_OUT_{$id} [Error] {error}" => 'error',
         ];
 
         $logger = Container::getInstance()->get('log');
